1f290cdc2236f695c867ec2fded41c111f9b16e1
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-03-07 04:52:32 +0000

    add doerinput component

---
+++ to-doer/screens/DoerInput.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const DoerInput = () => {
+    return (
+        <View style={StyleSheet.container}>
+            <Text>HI</Text>
+        </View>
+    );
+};
+export default DoerInput;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FBFCFD",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
