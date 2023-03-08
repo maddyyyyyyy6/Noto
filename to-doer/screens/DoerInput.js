78637fe6dc19f39f88af4ff2f1bbdee59a0473cf
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-03-08 06:49:12 +0000

    Doer input

--- to-doer/screens/DoerInput.js
+++ to-doer/screens/DoerInput.js
@@ -1,9 +1,71 @@
 import { StyleSheet, Text, View } from "react-native";
-
-const DoerInput = () => {
+import { TextInput } from "react-native-gesture-handler";
+import { AntDesign } from "@expo/vector-icons";
+import { useState, useRef, useEffect } from "react";
+const DoerInput = ({ navigation }) => {
+    const [title, setTitle] = useState("");
+    const [note, setNote] = useState("");
+    const titleInputRef = useRef(null);
+    const handleBack = () => {
+        if (title) {
+            navigation.navigate("Home", {
+                doer: {
+                    title: title,
+                    note: note,
+                },
+            });
+        } else if (note) {
+            navigation.navigate("Home", {
+                doer: {
+                    note: note,
+                },
+            });
+        } else {
+            navigation.navigate("Home");
+        }
+    };
+    useEffect(() => {
+        // Focus on the title input when the component mounts
+        titleInputRef.current?.focus();
+    }, []);
     return (
-        <View style={StyleSheet.container}>
-            <Text>HI</Text>
+        <View style={styles.container}>
+            <View style={styles.InputContainer}>
+                {/* Header */}
+                <View style={styles.headerContainer}>
+                    <AntDesign
+                        name="arrowleft"
+                        size={28}
+                        color="black"
+                        style={{ marginRight: 10 }}
+                        onPress={() => handleBack()}
+                    />
+                    <Text style={styles.headerText}>New Doer?</Text>
+                </View>
+                {/* Input */}
+                <View style={styles.NewDoerContainer}>
+                    <TextInput
+                        ref={titleInputRef}
+                        style={styles.inputTitle}
+                        placeholder="Title"
+                        multiline={true}
+                        numberOfLines={2}
+                        textAlignVertical="top"
+                        maxLength={100}
+                        onChangeText={setTitle}
+                        value={title}
+                    ></TextInput>
+                    <TextInput
+                        style={styles.inputNote}
+                        placeholder="Note"
+                        multiline={true}
+                        numberOfLines={11}
+                        textAlignVertical="top"
+                        onChangeText={setNote}
+                        value={note}
+                    ></TextInput>
+                </View>
+            </View>
         </View>
     );
 };
@@ -12,8 +74,48 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#FBFCFD",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    InputContainer: {
+        flex: 1,
+        backgroundColor: "#FBFCFD",
         width: "100%",
         padding: 15,
         paddingTop: 30,
+    },
+    headerContainer: {
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        flexDirection: "row",
+        height: 50,
+    },
+    headerText: {
+        fontFamily: "Inter_500Medium",
+        fontSize: 21,
+    },
+    NewDoerContainer: {
+        flex: 1,
+        width: "100%",
+        borderWidth: 1,
+        backgroundColor: "#fff",
+        padding: 10,
+        borderRadius: 11,
+        borderWidth: 1,
+        borderColor: "#DFE3E6",
+        marginBottom: 10,
+    },
+    inputTitle: {
+        fontFamily: "Inter_400Regular",
+        fontSize: 20,
+        textAlign: "left",
+        alignItems: "flex-start",
+        alignContent: "flex-start",
+        marginBottom: 20,
+    },
+    inputNote: {
+        fontFamily: "Inter_400Regular",
+        fontSize: 15,
     },
 });
