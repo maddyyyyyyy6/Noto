d641388a7a690bf6ecfe4e4f2b5853f676c1622e
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-03-08 06:49:20 +0000

    add doerviewer

---
+++ to-doer/screens/DoerView.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native/types";
+
+const DoerViewer = () => {
+    return (
+        <View>
+            <Text>Hi</Text>
+        </View>
+    );
+};
+
+export default DoerViewer;
