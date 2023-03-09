8316ae2568c347bd1c0acff7048abb67437d53d5
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-03-09 12:39:53 +0000

    code component

---
+++ to-doer/components/Code.js
@@ -0,0 +1,44 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Code({ title, code }) {
+    return (
+        <View style={styles.codeContainer}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}>{title}</Text>
+            </View>
+            <View style={styles.codeViewContainer}>
+                <Text style={styles.codeText}>{code}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    codeContainer: {
+        borderWidth: 1,
+        backgroundColor: "#fff",
+        borderRadius: 11,
+        borderWidth: 1,
+        borderColor: "#DFE3E6",
+        marginBottom: 10,
+        overflow: "hidden",
+    },
+    headerContainer: {
+        padding: 10,
+    },
+    headerText: {
+        fontSize: 20,
+    },
+    codeViewContainer: {
+        backgroundColor: "#3A3E4E",
+        paddingTop: 10,
+        paddingBottom: 20,
+        paddingHorizontal: 10,
+        color: "white",
+    },
+    codeText: {
+        color: "white",
+        fontSize: 14,
+    },
+});
