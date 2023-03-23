38014bbf89faf5c6e669a158845822e269bd36f0
tonyCoder08 <127786984+tonyCoder08@users.noreply.github.com>
2023-03-23 11:05:12 +0000

    todoconfig navigation

--- to-doer/navigation/index.js
+++ to-doer/navigation/index.js
@@ -8,97 +8,101 @@ import CodeInput from "../screens/CodeInput";
 import CodeViewer from "../screens/CodeViewer";
 import Todoer from "../screens/Todoer";
 import TODOInput from "../screens/TODOInput";
+import TODOconfig from "../screens/TODOconfig";
 
 const Stack = createStackNavigator();
 
 export default function MyStack() {
-    return (
-        <Stack.Navigator
-        screenOptions={{
-            headerShown: false,
-            gestureEnabled: true,
-            gestureDirection: "horizontal",
-            cardStyleInterpolator: ({ current, layouts }) => {
-              return {
-                cardStyle: {
-                  transform: [
-                    {
-                      translateX: current.progress.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [layouts.screen.width, 0],
-                      }),
-                    },
-                  ],
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        gestureEnabled: true,
+        gestureDirection: "horizontal",
+        cardStyleInterpolator: ({ current, layouts }) => {
+          return {
+            cardStyle: {
+              transform: [
+                {
+                  translateX: current.progress.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [layouts.screen.width, 0],
+                  }),
                 },
-              };
+              ],
             },
-          }}
-        >
-            <Stack.Screen
-                name="Home"
-                component={Home}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="Todoer"
-                component={Todoer}
-                options={{
-                    headerShown:false
-                }}
-
-            />
-            <Stack.Screen
-                name="TODOInput"
-                component={TODOInput}
-                options={{
-                    headerShown:false
-                }}
-
-            />
-            <Stack.Screen
-                name="DoerInput"
-                component={DoerInput}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="Viewer"
-                component={DoerViewer}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="Starred"
-                component={Starred}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="Codes"
-                component={Codes}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="CodeInput"
-                component={CodeInput}
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="CodeView"
-                component={CodeViewer}
-                options={{
-                    headerShown: false,
-                }}
-            />
-        </Stack.Navigator>
-    );
+          };
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Todoer"
+        component={Todoer}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="TODOInput"
+        component={TODOInput}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="TODOconfig"
+        component={TODOconfig}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="DoerInput"
+        component={DoerInput}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Viewer"
+        component={DoerViewer}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Starred"
+        component={Starred}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Codes"
+        component={Codes}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="CodeInput"
+        component={CodeInput}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="CodeView"
+        component={CodeViewer}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
